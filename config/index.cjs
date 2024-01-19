0a6feae0a74ea5034c166cd1fcb2c8724026bb65
luochang1 <102657606+luochang1@users.noreply.github.com>
2024-01-20 00:25:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'obQTe6b28pspj_MUtFWl-KqAB5dU',
+      id: 'obQTe6foRyVohSryOyjiS9n_p_WE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'DZPbhLRsD6JjU5qP6yRbH4AaxEfZS8yiRFdFckN5FaE',
+      useTemplateId: '4XfrsR1lvJobqRkiIXSXQUR9ijJEF4eOwLLtoxc1B9Y',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-28',
       festivals: [
